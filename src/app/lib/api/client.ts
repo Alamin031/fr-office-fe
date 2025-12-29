a7fc75e1475f9f84aebcd8a9f810fc440c2cb47c
Alamin031 <mridoy031@gmail.com>
2025-12-29 23:59:57 +0600

    tr

--- src/app/lib/api/client.ts
+++ src/app/lib/api/client.ts
@@ -8,7 +8,7 @@ import axiosRetry from 'axios-retry';
 
 const API_BASE_URL = (
   process.env.NEXT_PUBLIC_API_BASE_URL ||
-  'http://localhost:8080/api'
+  ' https://backend-production-8aca.up.railway.app/api'
 ).trim();
 
 export const apiClient: AxiosInstance = axios.create({
